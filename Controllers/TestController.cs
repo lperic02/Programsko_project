31afddf6fc6c6af85f59d5d04722dc1d5f033273
lperic02 <lukarius.peric@gmail.com>
2024-11-23 15:54:40 +0100

    project initialization

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Programsko.Controllers
+{
+
+    // /Test
+    public class TestController : Controller
+    {
+
+        // /Test
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Jozo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Band.cs
@@ -0,0 +1,14 @@
+﻿namespace Programsko.Models
+{
+    public class Band
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        
+        public string MusicGenre { get; set; }
+
+
+    }
+}
